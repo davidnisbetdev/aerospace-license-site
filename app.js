dd1aecbc04246f50a37636cde260cd49c4e7b3ae
David Nisbet <nizz3hx@protonmail.com>
2021-04-21 23:51:36 +0100

    Set up server, styling change on homepage.

---
+++ app.js
@@ -0,0 +1,30 @@
+const path = require('path')
+const express = require('express')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+const connectDB = require('./config/db')
+
+// Loading Config
+dotenv.config({ path: './config/config.env' })
+
+connectDB()
+
+const app = express()
+
+// Body Parser
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+if(process.env.NODE_ENV === 'dev'){
+    app.use(morgan('dev'))
+}
+
+const PORT = process.env.PORT || 3000
+
+app.listen(
+    PORT, 
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+    )
